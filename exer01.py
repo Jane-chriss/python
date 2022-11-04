a8cd3cf1d4266869f5fd8d401324090b52b698c8
Jane-chriss <117416110+Jane-chriss@users.noreply.github.com>
2022-11-03 22:02:37 -0300

    Add files via upload

---
+++ exer01.py
@@ -0,0 +1,6 @@
+#caracter de comentario
+#print para imprimir informações na tela
+print('ola mundo python')
+print('Meu nome é: ' , 'Jane, ', 'tenho,', 40, 'anos')
+
+
---
+++ exer02.py
@@ -0,0 +1,3 @@
+#usando input para entrada de dados
+print('Seja bem vindo a aula de ' + 'Payton')
+print('ola {}' . format (input ('qual o seu nome ')))
