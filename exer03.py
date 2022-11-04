b8f58a68a98a10c3328eb2270ac621dabf1e16bb
Jane-chriss <117416110+Jane-chriss@users.noreply.github.com>
2022-11-03 22:26:20 -0300

    Add files via upload

---
+++ exer03.py
@@ -0,0 +1,12 @@
+#trabalhando com variaveis
+#num1 = 10
+#num2 = 5
+
+num1 = input('informe o primeiro valor:')
+num2 = input('informe o primeiro valor:')
+
+soma = num1 + num2
+print('A soma é: ', soma)
+
+soma1 = int(num1) + int(num2)
+print('A soma correta é: {}' . format (soma1))
