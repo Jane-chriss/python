ceffdc81007e6bf2c883a94122cfa985d80ccb35
Jane-chriss <117416110+Jane-chriss@users.noreply.github.com>
2022-11-10 12:49:19 -0300

    Update exer09.py

--- exer09.py
+++ exer09.py
@@ -1,6 +1,4 @@
-#vamos trabalhar com dados de texto,
-#usando varios metiodos para verificar 
-#e modificar o valor de uma variavel de clss srt
+#vamos trabalhar com dados de texto, usando varios metiodos para verificar e modificar o valor de uma variavel de clss srt
 
 from os import system
 system('cls')
@@ -11,11 +9,9 @@ nomecompleto = input ('informe seu nome completo ')
 print ('1. total de caracteres:', len(nomecompleto))
 
 #acessando caracter a partir da posição
-
 print ('2. o caracter da posição 2 é: ' ,nomecompleto [2])
 
 #transformando maiuscula | minuscula
-
 print ('3. texto em maiuscula', nomecompleto.upper())
 print ('4. texto em maiuscula', nomecompleto.lower())
 print ('5. texto em maiuscula', nomecompleto.capitalize())
@@ -23,13 +19,11 @@ print ('5. texto em maiuscula', nomecompleto.capitalize())
 #apurar a posi~çao de um determinado caracter
 print ('6. posição do caracter espaço ' , nomecompleto.find (' '))
 
-
 #fatiar uma string ate uma determinada posição
 espaco = nomecompleto.find (' ')
 print ('7. somente o primeiro nome: ' , nomecompleto[0:espaco])
 
 #substituir um caracter por outro
-
 print ('8. nome sem espaço: ' , nomecompleto.replace(' ', ''))
 
 #verificar somente letra e numeros
