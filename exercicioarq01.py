e95bb4e72b42d3c23cc8e306b817161358c4d910
Jane-chriss <117416110+Jane-chriss@users.noreply.github.com>
2022-11-29 22:25:54 -0300

    Add files via upload

---
+++ exercicioarq01.py
@@ -0,0 +1,42 @@
+from os import system
+import os.path, datetime
+
+arquivo = 'produtos.csv'
+
+if os.path.isfile(arquivo):
+    produtos = open(arquivo, 'r', encoding='utf-8')
+    tamanho = os.path.getsize(arquivo)
+    datamodificacao = os.path.getatime(arquivo)
+    print(f'data de modificação: {datetime.datetime.fromtimestamp(datamodificacao)}')
+    print(f'tamanho do arquivo(bytes): {tamanho}')
+
+    listaprodutos = []
+    for linha in produtos:
+        colunas = linha.strip().split(";")
+        print(colunas)
+        colunas[0] = int(colunas[0])
+        colunas[2] = int(colunas[2])
+        colunas[4] = int(colunas[4])
+        colunas[5] = int(colunas[5])
+        colunas[6] = int(colunas[6])
+        colunas[7] = int(colunas[7])
+        colunas[8] = float(colunas[8])
+        colunas[9] = float(colunas[9])
+
+        listaprodutos.append(colunas)
+    produtos.close()
+    for prod in listaprodutos:
+        print(prod)
+
+#calculando total de custo
+    totaldecusto = 0
+    for prod in listaprodutos:
+        totaldecusto +=prod[9]
+    print(f'total de custo: {totaldecusto}')
+    
+
+
+
+
+
+
